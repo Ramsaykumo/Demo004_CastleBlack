75c23310805b9cbbe69f5f549b1118597ae42f88
Jackson <38564443+Ramsaykumo@users.noreply.github.com>
2019-10-07 15:28:02 +0800

    Upload the Scripts.
    此项目仅作纪念。

---
+++ Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Hero : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public Animator anim;
+    public ETCJoystick joy;
+	void Update () {
+        
+        
+	}
+
+    public void MoveAnimStart()
+    {
+        anim.SetFloat("HeroSpeed", joy.tmSpeed);
+    }
+
+    public void MoveAnimEnd()
+    {
+        anim.SetFloat("HeroSpeed", 0);
+    }
+}
